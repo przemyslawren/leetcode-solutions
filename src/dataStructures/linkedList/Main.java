3d4b4548dbffd77457cbe6d0d216f3783080e4b9
przemyslawren <przemyslaw.ren@gmail.com>
2024-08-06 08:48:08 +0200

    initial impl of LinkedList

---
+++ src/dataStructures/linkedList/Main.java
@@ -0,0 +1,14 @@
+package dataStructures.linkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(4);
+
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        myLinkedList.printList();
+    }
+}
